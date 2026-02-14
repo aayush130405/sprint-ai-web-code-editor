dae0add0e5f9afe803f7771b5fc0a1a4bb12c990
Aayush Yadav <aayush130405@gmail.com>
2026-02-14 23:41:14 +0530

    create root layout files and home page.tsx file

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HomeLayout ({children} : {children: React.ReactNode}) {
+    return (
+        <>
+            {/* header */}
+            {/* background effects and grid*/}
+            {/* main */}
+            {/* footer */}
+        </>
+    )
+}
