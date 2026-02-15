2843cc05c30ecfb56bb41e7e4f148bb2feae6b5a
Aayush Yadav <aayush130405@gmail.com>
2026-02-15 12:22:04 +0530

    create root page.tsx and layout.tsx file and create boilerplate header and footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,34 @@
+import Footer from "@/modules/home/footer";
+import Header from "@/modules/home/header";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+    title: {
+        template: "Sprint Editor",
+        default: "AI based web IDE"
+    }
+}
 
 export default function HomeLayout ({children} : {children: React.ReactNode}) {
     return (
         <>
             {/* header */}
+            <Header/>
+
             {/* background effects and grid*/}
+
+
             {/* main */}
+            <main className="z-20 relative w-full pt-0">
+                {
+                    children
+                }
+            </main>
+
+
             {/* footer */}
+            <Footer/>
         </>
     )
 }
