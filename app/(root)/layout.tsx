cd8b74804a85aca44db6898246018623844c487c
Aayush Yadav <aayush130405@gmail.com>
2026-02-18 23:22:04 +0530

    minor redesign home page and add grid pattern in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
-import Footer from "@/modules/home/footer";
+import { cn } from "@/lib/utils";
+import {Footer} from "@/modules/home/footer";
 import {Header} from "@/modules/home/header";
 import { Metadata } from "next";
 import React from "react";
@@ -17,6 +18,15 @@ export default function HomeLayout ({children} : {children: React.ReactNode}) {
             <Header/>
 
             {/* background effects and grid*/}
+            <div
+                className={cn(
+                    "absolute inset-0",
+                    "[background-size:40px_40px]",
+                    "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+                    "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+                )}
+            />
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"/>
 
 
             {/* main */}
