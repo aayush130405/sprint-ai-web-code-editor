bb640ac8dfc9e0d18c4a53b1b3dee1a4c5607067
Aayush Yadav <aayush130405@gmail.com>
2026-02-20 23:24:07 +0530

    add dashboard page and layout and configure layout settings

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default async function DashboardLayout ({children} : {children: React.ReactNode}) {
+    return (
+        <SidebarProvider>
+            <div className="flex min-h-screen w-full overflow-x-hidden">
+                {/* dashboard sidebar */}
+                <main className="flex-1">
+                    {children}
+                </main>
+            </div>
+        </SidebarProvider>
+    )
+}
