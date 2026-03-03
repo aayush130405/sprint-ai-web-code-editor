caa5b92690a7d13346f01a2dd5ffab3ee3a7c5dd
Aayush Yadav <aayush130405@gmail.com>
2026-03-03 17:24:48 +0530

    add create-playground method

--- modules/dashboard/actions/index.ts
+++ modules/dashboard/actions/index.ts
@@ -6,20 +6,45 @@ import { db } from "@/lib/db";
 import { getCurrentUser } from "@/modules/auth/actions";
 
 export const getAllPlaygroundForUser = async () => {
-    const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-    try {
-        const playground = await db.playground.findMany({
-            where: {
-                userId: user?.id
-            },
-            include: {
-                user: true
-            }
-        })
+  try {
+    const playground = await db.playground.findMany({
+      where: {
+        userId: user?.id,
+      },
+      include: {
+        user: true,
+      },
+    });
 
-        return playground;
-    } catch (error) {
-        console.log(error);
-    }
-}
+    return playground;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createPlayground = async (data: {
+  title: string;
+  template: "REACT" | "NEXTJS" | "EXPRESS" | "VUE" | "HONO" | "ANGULAR";
+  description?: string;
+}) => {
+  const user = await getCurrentUser();
+
+  const { title, template, description } = data;
+
+  try {
+    const playground = await db.playground.create({
+      data: {
+        title: title,
+        description: description,
+        template: template,
+        userId: user?.id!,
+      },
+    });
+
+    return playground;
+  } catch (error) {
+    console.log(error);
+  }
+};
