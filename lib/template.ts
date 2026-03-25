a6a1f4f7084c2e0cd890113e3737b4f0ecbb0fb2
Aayush Yadav <aayush130405@gmail.com>
2026-03-25 19:14:28 +0530

    create starter templates and path to json utility

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePath = {
+    REACT: "/starters-main/react-ts",
+    NEXTJS: "/starters-main/nextjs-new",
+    EXPRESS: "/starters-main/express-simple",
+    VUE: "/starters-main/vue",
+    HONO: "/starters-main/hono-nodejs-starter",
+    ANGULAR: "/starters-main/angular",
+}
