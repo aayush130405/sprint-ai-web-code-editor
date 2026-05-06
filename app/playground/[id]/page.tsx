7790e82ff58a4188957e811fb4b20d7907a73e92
Aayush Yadav <aayush130405@gmail.com>
2026-05-06 23:56:13 +0530

    start playground ui

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 import { usePlayground } from '@/modules/playground/hooks/usePlayground';
+import { TooltipProvider } from '@/components/ui/tooltip';
 import { useParams } from 'next/navigation'
 import React from 'react'
+import { SidebarInset, SidebarSeparator, SidebarTrigger } from '@/components/ui/sidebar';
+import { Separator } from '@/components/ui/separator';
 
 const MainPlaygroundPage = () => {
     const {id} = useParams<{id:string}>();
@@ -11,9 +14,25 @@ const MainPlaygroundPage = () => {
     console.log("playground data", playgroundData);
     
   return (
-    <div>
-        Params: {id}
-    </div>
+    <TooltipProvider>
+      <>
+        {/* Template file tree */}
+        <SidebarInset>
+          <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
+            <SidebarTrigger className='-ml-1'/>
+            <Separator orientation='vertical' className='mr-2 h-4'/>
+          </header>
+
+          <div className='flex flex-1 items-center gap-2'>
+            <div className='flex flex-col flex-1'>
+              <h1 className='text-sm font-medium'>
+                {playgroundData?.title || "Code playground"} 
+              </h1>
+            </div>
+          </div>
+        </SidebarInset>
+      </>
+    </TooltipProvider>
   )
 }
 
