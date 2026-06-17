a932f96e06e72a4b1e8471b5f6994906bffba6fb
Aayush Yadav <aayush130405@gmail.com>
2026-06-17 21:35:30 +0800

    install webcontainers/api and define hook and implement interfaces

--- modules/webcontainers/hooks/useWebContainer.ts
+++ modules/webcontainers/hooks/useWebContainer.ts
@@ -7,14 +7,14 @@ interface UseWebContainerProps {
 }
 
 interface UseWebContainerReturn {
-    serverUrl: string | null;
+    serverUrl: string | null;       //will return a url which will be used to display preview using iframe
     error: string | null;
     instance: WebContainer | null;
     isLoading: boolean;
-    writeFileSync: (path: string, content: string) => void;
+    writeFileSync: (path: string, content: string) => void; //will update the code in the container whenever changes are made in the editor
     destory: () => void;
 }
 
 export const useWebContainer = ({templateData}: UseWebContainerProps): UseWebContainerReturn => {
-
+    
 }
