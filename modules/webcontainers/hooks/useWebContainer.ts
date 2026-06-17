f55d7523564e9acf9fdc8d334561e9572f5b09d1
Aayush Yadav <aayush130405@gmail.com>
2026-06-17 21:51:30 +0800

    create state variable and create useEffect hook used to initialize web container, update state variables and then destory instance

--- modules/webcontainers/hooks/useWebContainer.ts
+++ modules/webcontainers/hooks/useWebContainer.ts
@@ -16,5 +16,38 @@ interface UseWebContainerReturn {
 }
 
 export const useWebContainer = ({templateData}: UseWebContainerProps): UseWebContainerReturn => {
-    
+    const [serverUrl, setServerUrl] = useState<string | null>(null);
+    const [error, setError] = useState<string | null>(null);
+    const [instance, setInstance] = useState<WebContainer | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        let mounted = true;
+        
+        async function initializeWebContainer() {
+            try {
+                const webcontainerInstance = await WebContainer.boot();
+
+                if(!mounted) return;
+
+                setInstance(webcontainerInstance);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Failed to initialize Web Container : ", error);
+                if(mounted) {
+                    setError(error instanceof Error ? error.message : "Failed to initialize Web Container");
+                    setIsLoading(false);
+                }
+            }
+        }
+
+        initializeWebContainer();
+
+        return () => {
+            mounted = false;
+            if(instance) {
+                instance.teardown();        //used to destroy the web container instance
+            }
+        }
+    }, [])
 }
