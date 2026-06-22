abec9f44fe8df854902a24bb38a737e71884d4b7
Aayush Yadav <aayush130405@gmail.com>
2026-06-22 16:51:03 +0800

    create:  web container preview component, transformer hook used to change templateData to webContainer format. modify: playground/page.tsx to include webContainerPreview component

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -21,8 +21,9 @@ import { Button } from '@/components/ui/button';
 import { Bot, FileText, Save, Settings, X } from 'lucide-react';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 import PlaygroundEditor from '@/modules/playground/components/playground-editor';
+import { useWebContainer } from '@/modules/webcontainers/hooks/useWebContainer';
 
 const MainPlaygroundPage = () => {
     const {id} = useParams<{id:string}>();
@@ -42,6 +43,15 @@ const MainPlaygroundPage = () => {
       openFiles,
       closeFile
     } = useFileExplorer();
+
+    const {
+      serverUrl, 
+      isLoading: containerLoading, 
+      error: containerError, 
+      instance, 
+      writeFileSync, 
+      //@ts-ignore
+    } = useWebContainer({templateData});
 
     useEffect(() => {setPlaygroundId(id)}, [id, setPlaygroundId]);
 
@@ -201,6 +211,26 @@ const MainPlaygroundPage = () => {
                           onContentChange={() => {}}
                           />
                         </ResizablePanel>
+
+                        {
+                          isPreviewVisible && (
+                            <>
+                              <ResizableHandle />
+                              <ResizablePanel defaultSize={50}>
+                                {/* render web container preview component */}
+                                <WebContainerPreview 
+                                templateData={templateData}
+                                instance={instance}
+                                writeFileSync={writeFileSync}
+                                isLoading={containerLoading}
+                                error={containerError}
+                                serverUrl={serverUrl}
+                                forceResetup={false}
+                                />
+                              </ResizablePanel>
+                            </>
+                          )
+                        }
                       </ResizablePanelGroup>
                   </div>
                 </div>
