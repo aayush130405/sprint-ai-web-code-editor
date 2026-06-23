4920fad3b36a46aba93e1ad10d90950d2190df5e
Aayush Yadav <aayush130405@gmail.com>
2026-06-23 18:02:22 +0800

    check if package.json exists and if yes then restore setup

--- modules/webcontainers/components/webcontainer-preview.tsx
+++ modules/webcontainers/components/webcontainer-preview.tsx
@@ -37,8 +37,49 @@ const WebContainerPreview = ({
     const [currentStep, setCurrentStep] = useState(0);
     const totalSteps = 4;
     const [setupError, setSetupError] = useState<string | null>(null);
-    const [setupComplete, setIsSetupComplete] = useState(false);
+    const [isSetupComplete, setIsSetupComplete] = useState(false);
     const [isSetupInProgress, setIsSetupInProgress] = useState(false);
+
+    useEffect(() => {
+        async function setupContainer() {
+            if(!instance || isSetupInProgress || isSetupComplete) return;
+
+            try {
+                setIsSetupInProgress(true);
+                setSetupError(null);
+
+                try {
+                    const packageJsonExists = await instance.fs.readFile('package.json', "utf-8");
+                    if(packageJsonExists) {
+                        //no need to setup again just restore previous one
+                        //TODO: terminal logic
+                        instance.on('server-ready', (port: number, url: string) => {
+                            //TODO: terminal logic
+
+                            setPreviewUrl(url);
+                            setLoadingState((prev) => ({        
+                                ...prev,
+                                starting: false,
+                                ready: true
+                            }))
+                            /*
+                                `prev` contains the current state before the update. The spread operator (`...prev`)
+                                copies all the existing state values into a new object. Then we only change the values
+                                we want (`starting` and `ready`). This means all other state properties stay exactly
+                                the same, and only these two fields are updated.
+                            */
+                        })
+                    }
+                } catch (error) {
+                    
+                }
+            } catch (error) {
+                
+            }
+        }
+
+        setupContainer();
+    }, [instance, templateData, isSetupComplete, isSetupInProgress])
 
   return (
     <div>WebContainerPreview</div>
