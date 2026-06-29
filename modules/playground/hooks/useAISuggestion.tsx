81fd75b99e8f4469849b3bb1182d8e5019e2e9b2
Aayush Yadav <aayush130405@gmail.com>
2026-06-29 18:20:13 +0800

    refactor: enhance suggestion handling in useAISuggestions hook with accept, reject, and clear functionalities

--- modules/playground/hooks/useAISuggestion.tsx
+++ modules/playground/hooks/useAISuggestion.tsx
@@ -100,7 +100,72 @@ export const useAISuggestions = (): useAISuggestionsReturn => {
 
     //used to get the suggestion from AI and insert in the codebase, remove decorations and later remove the suggestion from the memory
     const acceptSuggestion = useCallback(() => {
+        (editor: any, monaco: any) => {
+            setState((currentState) => {
+                if(!currentState.suggestion || !currentState.position || !editor || !monaco) {
+                    return currentState;
+                }
 
+                const {line, column} = currentState.position;
+                const sanitizedSuggestion = currentState.suggestion.replace(/^\d+:\s*/gm, "");  //making suggestion monaco compatible
+
+                editor.executeEdits("", [
+                    {
+                        range: new monaco.Range(line, column, line, column),
+                        text: sanitizedSuggestion,
+                        forceMoveMarkers: true,
+                    }
+                ]);
+
+                if(editor && currentState.decoration.length > 0) {
+                    editor.deltaDecorations(currentState.decoration, []);
+                } 
+
+                return {
+                    ...currentState,
+                    suggestion: null,
+                    position: null,
+                    decoration: []
+                }
+            })
+        }
     }, [])
 
+    const rejectSuggestion = useCallback((editor:any)=>{
+            setState((currentState)=>{
+                 if(editor && currentState.decoration.length > 0){
+                    editor.deltaDecorations(currentState.decoration , [])
+                }
+
+                return {
+                    ...currentState,
+                    suggestion:null,
+                    position:null,
+                    decoration:[]
+                }
+            })
+    },[]);
+ 
+    const clearSuggestion = useCallback((editor: any) => {
+        setState((currentState) => {
+          if (editor && currentState.decoration.length > 0) {
+            editor.deltaDecorations(currentState.decoration, []);
+          }
+          return {
+            ...currentState,
+            suggestion: null,
+            position: null,
+            decoration: [],
+          };
+    });
+  }, []);
+
+  return {
+    ...state,
+    toggleEnabled,
+    fetchSuggestion,
+    acceptSuggestion,
+    rejectSuggestion,
+    clearSuggestion
+  }
 }
